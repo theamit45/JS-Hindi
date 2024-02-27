983e8c5b07b74785a51fb0e3b419a458778e9964
theamit45 <160478323+theamit45@users.noreply.github.com>
2024-02-27 07:16:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "amit@gmail.com"
+var accountPassword = "09876"
+accountCity = "GKP"
+let accountState
+
+// accountId = 2 //  not Allowed
+accountEmail="amit@123gmail.com"
+accountPassword="7965453"
+accountCity="Punjab"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
