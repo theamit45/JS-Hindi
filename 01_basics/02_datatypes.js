c298081cff73eca4146c95a2f779de4bca4e807e
theamit45 <160478323+theamit45@users.noreply.github.com>
2024-02-27 14:18:11 +0000

    conversion operation

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all js code as newer version
+
+// aleart(2+4) we are using node js not browser
+let name = "amit"   //string data type
+let age = 16        //int data type/number
+let isLoggedIn = true //bool 
+
+//types of datatypes
+/*
+Number
+String
+BigInt
+Boolean
+null=standalone value
+undefined
+symbol=unique
+*/
+console.log(typeof "amit")
+console.log(typeof age)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// let score2="12qwe"
+// console.log(typeof score);
+// let valueInNumber2= Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2);
+
+/* operations*/
+let value=10
+let negValue=-value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="amit"
+let str2=" maurya"
+
+console.log(str1+str2)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+console.log(+true)
+console.log(+false)
+console.log(+"")
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
