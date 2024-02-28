d02f98e15add84f160252b5be8c26aa4102690c8
theamit45 <160478323+theamit45@users.noreply.github.com>
2024-02-28 20:15:54 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(1>2)
+// console.log(1<2)
+// console.log(1>=2)
+// console.log(1<=2)
+
+
+// console.log("1">=2)
+// console.log(1>"2")
+
+// console.log(null<0)
+// console.log(null<=0)
+// console.log(null>0)
+// console.log(null>=0)
+
+// equality check do not convert null into number but comparison convert the null into number
+
+// console.log(undefined<0)
+// console.log(undefined>0)
+// console.log(undefined==0)
+
+// === (strictly check) its check with data types also
+
+console.log("2"===2)
+console.log("2"==="2")
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// primitive(call by value)
+
+// 7 types: String,Number,Boolean,null,undefined,Symbol,BigIng.
+
+// Reference typeof/Non primitive(call by reference)
+// types: Array,Object,function
+
+const id = Symbol('123')
+const bigNum = 1234567654334567n
+
+const hero = ["loki","stark","thor"]  //array
+const obj = {           
+    name: "amit",
+    age: 22,
+}                               //object
